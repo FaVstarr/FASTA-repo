05dc1b049cfe07d5c97234428a4c668dd967eedb
Favour Chinemerem Chamberlain <favourchamberlain32@gmail.com>
2024-01-23 13:46:56 +0100

    resolve dependencies issues

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {  Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <Text classname="text-red-600">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
