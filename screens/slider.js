eee13202cea9c548be38820cee54a266ca475d28
Favour Chinemerem Chamberlain <favourchamberlain32@gmail.com>
2024-01-31 14:53:37 +0100

    added images

--- screens/slider.js
+++ screens/slider.js
@@ -1,6 +1,15 @@
 export default [
     {
         id: '1',
-        title: 'Quick delivery at your doorsteps'
+        title: 'Quick delivery at your doorsteps',
+        description: 'Enjoy quick pick-ups and deliveries at your door step',
+        image: require('/assets/images/In no time-pana 1.svg'),
+    },
+
+    {
+        id: '2',
+        title: 'Flexible payment',
+        description: 'Different modes of payment either before and after delivery without stress',
+        image: require('/assets/images/background-complete.svg'),
     }
 ]
