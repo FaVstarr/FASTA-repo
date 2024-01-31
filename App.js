12c2be669423293ecde3b645472f6e05d62b5f3d
Favour Chinemerem Chamberlain <favourchamberlain32@gmail.com>
2024-01-31 14:15:41 +0100

    made changes to fasta

--- App.js
+++ App.js
@@ -1,41 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Signup from './screens/Signup';
 import Signin from './screens/Signin';
+import Onboarding from './screens/Onboarding';
+import HomeScreen from './screens/HomeScreen';
+import DeliveriesScreen from './screens/DeliveriesScreen';
 
-const HomeScreen = ()=>{
-  return(
-    <View className="items-center justify-center flex-1">
-      <Text>
-        Home Screen
-      </Text>
-    </View>
-  )
-}
 
-const DeliveriesScreen = () =>{
-  return(
-    <View className="items-center justify-center flex-1">
-      <Text>
-          Deliveries
-      </Text>
-    </View>
-  )
-}
+
 
 const Stack = createNativeStackNavigator()
 
+
 export default function App() {
+  
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Signup' >
+        <Stack.Navigator initialRouteName='Onboarding' 
+        screenOptions={{headerShown: false}}
+        >
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Deliveries" component={DeliveriesScreen} />
           <Stack.Screen name="Signup" component={Signup}/>
           <Stack.Screen name="Signin" component={Signin}/>
+          <Stack.Screen name="Onboarding" component={Onboarding} />
         </Stack.Navigator>
     
     </NavigationContainer>
---
+++ screens/Onboarding.js
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+
+
+
+
+
+export default function Onboarding() {
+
+
+  return (
+    <SafeAreaView>
+      <Text>Onboarding</Text>
+    </SafeAreaView>
+  )
+}
