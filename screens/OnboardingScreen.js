c07ca7064eeaac0d7e6962b91fe6c012c886ca6e
Favour Chinemerem Chamberlain <favourchamberlain32@gmail.com>
2024-02-01 22:46:17 +0100

    onboarding

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,17 +1,32 @@
-import { View, Text, FlatList,Image} from 'react-native'
+import { View, Text, FlatList,Image, Button} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import Onboarding from 'react-native-onboarding-swiper';
 
+const Done =({...props}) => (
+  <Button className="bg-blue-500"
+  title='Sign in'
+  {...props}
+  />
+)
 
 
-export default function OnboardingScreen() {
 
+
+export default function OnboardingScreen({navigation}) {
+
+
+  
+  
 
   return (
     
-    <Onboarding className="flex-1 items-center justify-center"
+    <Onboarding className="flex-1 items-center justify-center my-[30px]" 
+    onSkip={()=> navigation.navigate("Signin")}
+    onDone={()=> navigation.navigate("Signin")}
+    DoneButtonComponent={Done}
+    
 
     pages={[
       {
@@ -29,8 +44,8 @@ export default function OnboardingScreen() {
       {
         backgroundColor: '#fff',
         image: <Image source={require('../assets/images/rafiki.png')} className=""  />,
-        title: <Text className="text-[20px] font-semibold pb-[50px] text-blue-500">Quick Delivery At Your Door Step</Text>,
-        subtitle: 'Done with React Native Onboarding Swiper',
+        title: <Text className="text-[20px] font-semibold pb-[50px] text-blue-500">Real-Time Tracking</Text>,
+        subtitle: 'Track your packages/items from the comfort of your home till final destination',
       },
     ]}
   />
