15b61b6dfa8a23088df76987e3b4f3fcd3b0b61e
Favour Chinemerem Chamberlain <favourchamberlain32@gmail.com>
2024-02-01 21:57:03 +0100

    added onboardingScreen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Signup from './screens/Signup';
 import Signin from './screens/Signin';
-import Onboarding from './screens/Onboarding';
+import OnboardingScreen from './screens/OnboardingScreen';
 import HomeScreen from './screens/HomeScreen';
 import DeliveriesScreen from './screens/DeliveriesScreen';
 
@@ -19,14 +19,14 @@ export default function App() {
   
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName='Onboarding' 
+        <Stack.Navigator initialRouteName='OnboardingScreen' 
         screenOptions={{headerShown: false}}
         >
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Deliveries" component={DeliveriesScreen} />
           <Stack.Screen name="Signup" component={Signup}/>
           <Stack.Screen name="Signin" component={Signin}/>
-          <Stack.Screen name="Onboarding" component={Onboarding} />
+          <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
         </Stack.Navigator>
     
     </NavigationContainer>
