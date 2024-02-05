6f7859789455d3a4e1af8abad61e43dc6fd23a75
Favour Chinemerem Chamberlain <favourchamberlain32@gmail.com>
2024-02-05 23:46:23 +0100

    changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,6 +30,23 @@ export default function HomeScreen() {
         <Image source={require('../assets/images/notification.png')}/>
       </TouchableOpacity>
         </View>
+
+        <View className="mt-[38px]">
+          <Text className="text-[#0560FA]">Special for you</Text>
+        </View>
+
+        <View className="mt-[90px]">  
+        <Text className="text-[#0560FA]">What will you like to do</Text>
+
+        <TouchableOpacity>
+          <View className="">
+            <Image source={require('../assets/images/healthicons_call-centre.png')} />
+            <Text className="text-[16px] text-[#0560FA]">Customer care</Text>
+            <Text className="text-[7.45px] block">Our customer care service line is available from 8 -9pm week days and 9 - 5 weekends - tap to call us today</Text>
+          </View>
+        </TouchableOpacity>
+        </View>
+        
        
     </SafeAreaView>
   );
