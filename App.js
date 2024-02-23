8eefa2891e9199854067e2dde21261c41a44e6d4
FaVstarr <favourchamberlain32@gmail.com>
2024-02-23 23:16:10 +0100

    made changes

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ export default function App() {
     return (
       <Tab.Navigator initialRouteName='Home' screenOptions={{headerShown: false}} labeled>
         <Tab.Screen name='Home' component={HomeScreen} options={{ tabBarIcon:({}) =>(<Entypo name="home" size={24} color="#0560FA" />)}} initialParams={{firstName: firstName, routeName: 'Home'}}/>
-        <Tab.Screen name='Wallet' component={Wallet} options={{ tabBarIcon:({}) =>(<Entypo name="wallet" size={24} color="#0560FA" />) }} initialParams={{firstName: firstName, lastName: lastName}} />
+        <Tab.Screen name='Wallet' component={Wallet} options={{ tabBarIcon:({}) =>(<Entypo name="wallet" size={24} color="#0560FA" />), headerShown: true }} initialParams={{firstName: firstName, lastName: lastName}} />
         <Tab.Screen name="Track" component={DeliveriesScreen} options={{ tabBarIcon:({}) =>(<MaterialIcons name="delivery-dining" size={24} color="#0560FA" />) }}  />
         <Tab.Screen name='Profile' component={Profile} options={{ tabBarIcon:({}) =>(<FontAwesome5 name="user-circle" size={24} color="#0560FA" />) }} />
       </Tab.Navigator>
